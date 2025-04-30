39967426fa9770d13fe9f63b71c5ec486a5eb1b3
aleqq123 <aleg.fidelus@gmail.com>
2025-04-30 08:43:18 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -39,6 +39,12 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+    },
+    "Chess Class": {
+        "description": "Physical education and sports activities",
+        "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
+        "max_participants": 30,
+        "participants": ["john@mergington.edu", "olivia@mergington.edu"]
     }
 }
 
